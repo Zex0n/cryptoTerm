becc415c73d2d1f4617453974ae388664e1fecf1
Trump <trumpk@gmail.com>
2017-08-15 16:05:39 +0300

    api to api_keys

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,7 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
+=begin
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
@@ -25,6 +26,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
+=end
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
